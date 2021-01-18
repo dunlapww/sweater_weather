ff49e8bc68d90dd58876b8bdc9ac1413bca60bec
Will Dunlap <dunlapww@gmail.com>
2021-01-17 21:56:41 -0700

    Improve status code bad login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: user_params[:email])
-    
+
     if user && user.authenticate(user_params[:password])
       user_w_key = User.find(user.id)
       session[:id] = user_w_key[:id]
       render json: UserSerializer.new(user_w_key)
     else
-      render json: {errors:[{detail: "Invalid credentials"}]}, status: :bad_request
+      render json: {errors:[{detail: "Invalid credentials"}]}, status: :not_found
     end
   end
 
