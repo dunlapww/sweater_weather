a3710407a4f3a930ee79fdb63a173a1f806a1f6f
Will Dunlap <dunlapww@gmail.com>
2021-01-18 15:11:11 -0700

    Clean up trip_facade

--- app/facades/trip_facade.rb
+++ app/facades/trip_facade.rb
@@ -6,13 +6,12 @@ class TripFacade
     dest_loc = trip_response[:route][:legs].last[:maneuvers].last[:startPoint]
     dest_weather = WeatherService.get_weather(dest_loc)
     dest_tz_offset = dest_weather[:timezone_offset]
-    current_time = Time.now
 
     trip_data = {
       start_city: trip_params[:origin],
       end_city: trip_params[:destination],
       travel_time: travel_time,
-      arrival_time: local_arrival_secs(current_time, dest_tz_offset, travel_time)
+      arrival_time: local_arrival_secs(dest_tz_offset, travel_time)
     }
 
     trip_data[:weather_at_eta] = arrival_weather(dest_weather, trip_data[:arrival_time])
@@ -28,9 +27,9 @@ class TripFacade
     eta_weather || dest_weather[:hourly].last
   end
 
-  def self.local_arrival_secs(local_time, dest_offset, travel_time)
-    utc_time = local_time.utc.strftime('%s').to_i
+  def self.local_arrival_secs(dest_offset, travel_time)
+    utc_time = Time.now.utc.strftime('%s').to_i
     dest_time_before_departure = utc_time + dest_offset
-    arrival_time = dest_time_before_departure + travel_time
+    dest_time_before_departure + travel_time
   end
 end
