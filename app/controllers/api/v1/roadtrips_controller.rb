48607b110e637a7cd99d67fcef0a0ae9c5a8a406
Will Dunlap <dunlapww@gmail.com>
2021-01-18 16:37:31 -0700

    add sad path tests and function for  create trip requests

--- app/controllers/api/v1/roadtrips_controller.rb
+++ app/controllers/api/v1/roadtrips_controller.rb
@@ -3,6 +3,10 @@ class Api::V1::RoadtripsController < ApplicationController
     trip_params = JSON.parse(request.body.read, symbolize_names: true)
     
     trip = TripFacade.get_trip(trip_params)
-    render json: TripSerializer.new(trip)
+    if trip.class == Trip
+      render json: TripSerializer.new(trip)
+    else
+      render json: trip, status: :bad_request
+    end
   end
 end
--- spec/requests/api/v1/roadtrips/create_spec.rb
+++ spec/requests/api/v1/roadtrips/create_spec.rb
@@ -18,6 +18,55 @@ RSpec.describe 'Search' do
       expect(response).to be_successful
       trip_data = JSON.parse(response.body, symbolize_names: true)
       expect(trip_data).to have_key(:data)
+      expect(trip_data[:data]).to be_a Hash
+      expect(trip_data[:data]).to have_key :id
+      expect(trip_data[:data]).to have_key :type
+      expect(trip_data[:data][:type]).to eq 'roadtrip'
+      expect(trip_data[:data]).to have_key :attributes
+      expect(trip_data[:data][:attributes]).to be_a Hash
+      expect(trip_data[:data][:attributes]).to have_key :start_city
+      expect(trip_data[:data][:attributes][:start_city]).to eq(trip_params[:origin])
+      expect(trip_data[:data][:attributes]).to have_key :end_city
+      expect(trip_data[:data][:attributes][:end_city]).to eq(trip_params[:destination])
+      expect(trip_data[:data][:attributes]).to have_key :travel_time
+      expect(trip_data[:data][:attributes][:travel_time]).to be_a String
+      expect(trip_data[:data][:attributes]).to have_key :weather_at_eta
+      expect(trip_data[:data][:attributes][:weather_at_eta]).to be_a Hash
+      expect(trip_data[:data][:attributes][:weather_at_eta]).to have_key :temperature
+      expect(trip_data[:data][:attributes][:weather_at_eta][:temperature]).to be_a(Numeric)
+      expect(trip_data[:data][:attributes][:weather_at_eta]).to have_key :conditions
+      expect(trip_data[:data][:attributes][:weather_at_eta][:conditions]).to be_a(String)
+      end
+  end
+  it 'returns unsuccessful response when given invalid data' do
+    VCR.use_cassette('road_trip_no_origin_request') do
+      trip_params = { origin: '',
+                      destination: 'Denver, CO',
+                      api_key: @user.api_key }
+
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      post '/api/v1/roadtrips', params: trip_params.to_json, headers: headers
+
+      resp = JSON.parse(response.body, symbolize_names: true)
+      expected = {:errors=>[{:detail=>"At least two locations must be provided."}]}
+      expect(resp).to eq(expected)
+      expect(response.status).to eq(400)
+    end
+    VCR.use_cassette('road_trip_bad_origin_request') do
+      trip_params = { origin: '/',
+                      destination: 'Denver, CO',
+                      api_key: @user.api_key }
+
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      post '/api/v1/roadtrips', params: trip_params.to_json, headers: headers
+
+      resp = JSON.parse(response.body, symbolize_names: true)
+      expected = {:errors=>[{:detail=>"Error processing request: Encountered an error while trying to batch geocode: Geocode Failed: A JSONObject text must begin with '{' at character 0 of "}]}
+      expect(resp).to eq(expected)
+      expect(response.status).to eq(400)
     end
   end
+
 end
