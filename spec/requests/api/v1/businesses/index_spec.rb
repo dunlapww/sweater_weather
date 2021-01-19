a7427e7e8dcc4f43bec352bcf426b2d2c7896bc1
Will Dunlap <dunlapww@gmail.com>
2021-01-19 09:11:21 -0700

    Add request spec for yelp

--- spec/requests/api/v1/businesses/index_spec.rb
+++ spec/requests/api/v1/businesses/index_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe 'when I receive a request to create a new user' do
   it "I'm able to consume the json body" do
     headers = { "CONTENT_TYPE" => "application/json"}
-    params = {email: 'person@woohoo.com',password: 'abc123',password_confirmation: 'abc123'}
+    params = {location: 'Pueblo, CO', search_term: 'tacos'}
     
-    get '/api/v1/users', params: params.to_json, headers: headers
+    get '/api/v1/businesses', params: params.to_json, headers: headers
     
    
     expect(response).to be_successful
