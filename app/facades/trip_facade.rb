e144281fe2e83b3a83eb86e405cf205fcf5e1c41
Will Dunlap <dunlapww@gmail.com>
2021-01-19 16:40:04 -0700

    more work on time

--- app/facades/trip_facade.rb
+++ app/facades/trip_facade.rb
@@ -9,16 +9,29 @@ class TripFacade
     dest_loc = trip_response[:route][:legs][0][:maneuvers].last[:startPoint]
     dest_weather = WeatherService.get_weather(dest_loc)
     dest_tz_offset = dest_weather[:timezone_offset]
+    utc_arrival_time = Time.now + travel_time
 
     trip_data = {
       start_city: trip_params[:origin],
       end_city: trip_params[:destination],
       travel_time: travel_time,
-      arrival_time: local_arrival_secs(dest_tz_offset, travel_time)
+      arrival_time: local_arrival_time(travel_time, dest_tz_offset)
     }
 
-    trip_data[:weather_at_eta] = arrival_weather(dest_weather, trip_data[:arrival_time])
-    Trip.new(trip_data)
+    trip_data[:weather_at_eta] = arrival_weather(dest_weather, utc_arrival_time)
+    trip = Trip.new(trip_data)
+
+    ######
+    arrival_time = (Time.now + travel_time).strftime('%s')
+    utc_arrival_time = (Time.now.utc + travel_time).strftime('%s')
+    counter = 0
+    weather_times = dest_weather[:hourly].map do |hour|
+      counter +=1
+      "#{counter}: #{hour[:dt]}"
+    end
+    weather_hour = trip.weather_at_eta.time.strftime('%s')
+    ####
+    require 'pry'; binding.pry
   end
 
   def self.travel_secs(trip_legs)
@@ -27,16 +40,23 @@ class TripFacade
     end
   end
   
-  def self.arrival_weather(dest_weather, arrival_time)
+  def self.arrival_weather(dest_weather, utc_arrival_time)
+    counter = 0
     eta_weather = dest_weather[:hourly].find do |hour|
-      arrival_time >= hour[:dt] && arrival_time <= hour[:dt] + 3599
+      counter += 1
+      utc_arrival_time >= hour[:dt] && arrival_time <= hour[:dt] + 3599
     end
-
+    require 'pry'; binding.pry
+    if eta_weather
+      eta_weather
+    else
+      eta_weather = dest_weather[:hourly].last
+    end
     eta_weather || dest_weather[:hourly].last
   end
 
-  def self.local_arrival_secs(dest_offset, travel_time)
-    utc_time = Time.now.utc.strftime('%s').to_i
-    utc_time + dest_offset + travel_time
+  def self.local_arrival_time(travel_time, destination_utc_offset)
+    local_time = Time.now.utc + travel_time + destination_utc_offset
+    local_time.strftime("%Y-%m-%d %H:%M %p (local time)")
   end
 end
--- app/poros/trip.rb
+++ app/poros/trip.rb
@@ -9,7 +9,7 @@ class Trip
   def initialize(trip_data)
     @start_city = trip_data[:start_city]
     @end_city = trip_data[:end_city]
-    @loc_arr_time = Time.at(trip_data[:arrival_time])
+    @loc_arr_time = trip_data[:arrival_time]
     @travel_time = to_hours_mins(trip_data[:travel_time])
     @weather_at_eta = WeatherHour.new(trip_data[:weather_at_eta])
   end
