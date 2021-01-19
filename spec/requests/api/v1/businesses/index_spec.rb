c20493c392f880f407210f6d01574e580a67f264
Will Dunlap <dunlapww@gmail.com>
2021-01-19 09:22:33 -0700

    cannot get params or anything to pass to controller

--- spec/requests/api/v1/businesses/index_spec.rb
+++ spec/requests/api/v1/businesses/index_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 describe 'when I receive a request to create a new user' do
   it "I'm able to consume the json body" do
-    VCR.use_cassette('pueblo_yelp_search') do
-      headers = { "CONTENT_TYPE" => "application/json"}
-      params = {location: 'Pueblo, CO', search_term: 'tacos'}
+    #VCR.use_cassette('pueblo_yelp_search') do
+      #headers = { "CONTENT_TYPE" => "application/json"}
+      biz_params = {location: 'Pueblo, CO', search_term: 'tacos'}
       
-      get '/api/v1/businesses', params: params.to_json, headers: headers
+      get '/api/v1/businesses', params: biz_params
       
-     
       expect(response).to be_successful
-    end
+    #end
   end
 end
