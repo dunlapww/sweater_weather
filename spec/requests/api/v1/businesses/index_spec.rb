3c9549c9fd638145c9b9b01d2ab2dd54c773f702
Will Dunlap <dunlapww@gmail.com>
2021-01-19 09:13:22 -0700

    update yelp spec

--- spec/requests/api/v1/businesses/index_spec.rb
+++ spec/requests/api/v1/businesses/index_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 describe 'when I receive a request to create a new user' do
   it "I'm able to consume the json body" do
-    headers = { "CONTENT_TYPE" => "application/json"}
-    params = {location: 'Pueblo, CO', search_term: 'tacos'}
-    
-    get '/api/v1/businesses', params: params.to_json, headers: headers
-    
-   
-    expect(response).to be_successful
+    VCR.use_cassette('pueblo_yelp_search') do
+      headers = { "CONTENT_TYPE" => "application/json"}
+      params = {location: 'Pueblo, CO', search_term: 'tacos'}
+      
+      get '/api/v1/businesses', params: params.to_json, headers: headers
+      
+     
+      expect(response).to be_successful
+    end
+  end
+end
