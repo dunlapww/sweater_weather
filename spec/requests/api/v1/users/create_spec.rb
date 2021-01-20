3923b1426d8779cf34561fd651bda43080992567
Will Dunlap <dunlapww@gmail.com>
2021-01-19 23:41:38 -0700

    Add readme images

--- spec/requests/api/v1/users/create_spec.rb
+++ spec/requests/api/v1/users/create_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'when I receive a request to create a new user' do
   it "I'm able to consume the json body" do
-    headers = { "CONTENT_TYPE" => "application/json"}
+    headers = { "Content-Type"=> "application/json", "Accept"=> "application/json"}
     params = {email: 'person@woohoo.com',password: 'abc123',password_confirmation: 'abc123'}
     #below passes in data in request.body, but also in params
     #post '/api/v1/users', params: params.to_json
